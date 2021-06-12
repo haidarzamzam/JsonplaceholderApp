eed20fe2a1013835c6c79416623372ffc6d87e18
Haidar Zamzam <haidar.zamzam99@gmail.com>
2021-06-12 20:58:41 +0700

    :sparkles: Add lib and init main

---
+++ lib/app.dart
@@ -0,0 +1,48 @@
+import 'package:case_devido/utils/toast.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/bloc_delegate.dart';
+
+class App {
+  final String apiBaseURL;
+  final String appTitle;
+  static App _instance;
+  Dio dio;
+
+  App.configure({this.apiBaseURL, this.appTitle, this.dio}) {
+    _instance = this;
+  }
+
+  factory App() {
+    if (_instance == null) {
+      throw UnimplementedError("App must be configured first.");
+    }
+
+    return _instance;
+  }
+
+  Future<Null> init() async {
+    // configure bloc delegate
+    BlocSupervisor.delegate = SimpleBlocDelegate();
+
+    dio = Dio(BaseOptions(
+        baseUrl: apiBaseURL,
+        connectTimeout: 10000,
+        receiveTimeout: 20000,
+        responseType: ResponseType.json));
+    setDioInterceptor();
+  }
+
+  void setDioInterceptor() {
+    dio.interceptors.add(InterceptorsWrapper(onError: (DioError e) async {
+      Map<String, dynamic> data = e.response.data;
+      if (e.response.statusCode != null) {
+        if (e.response.statusCode == 400) {
+          ToastUtils.show(data['message']);
+        }
+      }
+      return e;
+    }));
+  }
+}
---
+++ lib/utils/endpoint.dart
@@ -0,0 +1,3 @@
+class Endpoint {
+  static const String baseURL = "https://jsonplaceholder.typicode.com/";
+}
